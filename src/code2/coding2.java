112910adc19e3a1d93eec7a6e638fab779a9a5dd
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-07 14:42:51 +0530

    commit packages

---
+++ src/code2/coding2.java
@@ -0,0 +1,13 @@
+package code2;
+
+public class coding2 {
+	
+	public static void main(String[] args)  {
+		
+		String o = "fun with vaishnavi";
+		String h = "Hii Vaishnvi Jambhule";
+		
+		System.out.println(o);
+		System.out.println(h);
+	}
+}
