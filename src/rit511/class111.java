3b1d756c811258562ccb9df89636b05424fb64bd
vaishgithubrit <134505172+vaishgithubrit@users.noreply.github.com>
2023-11-08 17:15:23 +0530

    Update class111.java

--- src/rit511/class111.java
+++ src/rit511/class111.java
@@ -5,7 +5,10 @@ public class class111 {
 	public static void main(String[] args)   {
 		
 		String o = "hii I am Vaishnavi";
-		
+		int r = 9999;
+		int f = 888;
 		System.out.println(o);
+		System.out.println(r);
+		System.out.println(f);
 	}
 }
