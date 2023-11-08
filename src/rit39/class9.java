736c1eb6389ba07469c001a7b66e94bce414b4f1
vaishgithubrit <vaishnavijambhule18@gmail.com>
2023-11-08 16:23:35 +0530

    commit changes

---
+++ src/rit39/class9.java
@@ -0,0 +1,10 @@
+package rit39;
+
+public class class9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/rit49/rit10.java
@@ -0,0 +1,11 @@
+package rit49;
+
+public class rit10 {
+	
+	public static void main(String[] args)   {
+		
+		int y = 90;
+		
+		System.out.println(y);
+	}
+}
