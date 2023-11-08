2b90a914c63e2a4778179e032208120c8c08f9b4
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-08 17:53:10 +0530

    commit packages

---
+++ src/rit612/class612.java
@@ -0,0 +1,13 @@
+package rit612;
+
+public class class612 {
+	
+	public static void main(String[] args)   {
+		
+		int j = 88;
+		int k = 77;
+		
+		System.out.println(j);
+		System.out.println(k);
+	}
+}
